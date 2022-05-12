9cb843122ec4c5db36482088590de5c2a083f2b8
DubsOchenta <tylorjperry@gmail.com>
2022-05-11 21:37:36 -0700

    commerce site rough draft.

---
+++ chamber/js/toggleMenu.js
@@ -0,0 +1,11 @@
+function menuToggle() {
+    let x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  
+  const hamburger = document.getElementById('hamburger');
+  hamburger.addEventListener('click', menuToggle);
