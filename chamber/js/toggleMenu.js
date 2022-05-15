ffc9f5c1c710f4a36cf6a50f3d308381118c9986
DubsOchenta <tylorjperry@gmail.com>
2022-05-14 20:27:37 -0700

    updates to code

--- chamber/js/toggleMenu.js
+++ chamber/js/toggleMenu.js
@@ -1,13 +1,5 @@
-function menuToggle() {
-    let x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-  
-  const hamburger = document.getElementById('hamburger');
-  hamburger.addEventListener('click', menuToggle);
-  const element = document.getElementById("myLinks")
-  element.classList.toggle("myLinks");
+const hambutton = document.querySelector(".ham");
+const mainnav = document.querySelector(".navigation")
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
