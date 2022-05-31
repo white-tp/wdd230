b6298cf71af9deee0f3c2af900cef14883583596
DubsOchenta <tylorjperry@gmail.com>
2022-05-31 13:41:25 -0700

    format logo to .webp and update windchill.js

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -5,3 +5,9 @@ if (temperature >= 50 && windspeed >= 3.0) {
     let chill;
     chill = 35.74 + 0.6215*temperature - 35.75*windspeed^0.16 + 0.4275*temperature * windspeed^0.16;
 }
+else {
+    chill = NaN;
+}
+
+document.getElementById("chill").innerHTML += `Wind Chill: ${chill}&deg;`;
+document.getElementById("temperature").innerHTML += `Temperature: ${temperature}&deg;`;
