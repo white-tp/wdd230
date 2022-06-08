3f64d90f34a1af52a95381a460efdcc00991ff6a
DubsOchenta <tylorjperry@gmail.com>
2022-06-07 22:19:13 -0700

    fixed the weather app

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,14 +1,19 @@
-let temperature;
-let windspeed;
+let temperature= 80;
+let windspeed= 5;
+let chill;
 
 if (temperature >= 50 && windspeed >= 3.0) {
-    let chill;
-    chill = 35.74 + 0.6215*temperature - 35.75*windspeed^0.16 + 0.4275*temperature * windspeed^0.16;
+    chill = windChill(temperature, windspeed);
 }
 else {
-    chill = NaN;
+    chill = "N/A";
 }
 
-document.getElementById("chill").innerHTML += `${chill}&deg;`;
+function windChill(temperature, windspeed) {
+    wci = 35.74 + 0.6215*temperature - 35.75 * windspeed**0.16 + 0.4275 * temperature * windspeed**0.16;
+    return wci.toFixed(1);
+}
+
+document.getElementById("windspeed").innerHTML += `${windspeed} mph`;
 document.getElementById("temperature").innerHTML += `${temperature}&deg;`;
-document.getElementById("windspeed").innerHTML += `${windspeed} mi/hr`;
+document.getElementById("chill").innerHTML += `${chill}&deg;`;
