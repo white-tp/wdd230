616c45f9968af3ecf91180e2b185cdf636048cd7
DubsOchenta <tylorjperry@gmail.com>
2022-06-18 15:32:15 -0700

    directory page updated

---
+++ chamber/js/fetch.js
@@ -0,0 +1,42 @@
+const requestURL = 'JSON/data.json';
+const cards = document.querySelector('.cards');
+
+fetch(requestURL)
+  .then((response) => {
+    return response.json();
+  })
+  .then((jsonObject) => {
+    const companies = jsonObject['companies'];
+    companies.forEach(displayCompanies);
+  });
+
+const displayCompanies = (company) => {
+    // Create elements to add to the document
+    let card = document.createElement('section');
+    let h1 = document.createElement('h1');
+    let h2 = document.createElement('h2');
+    let h3 = document.createElement('h3');
+    let h4 = document.createElement('h4');
+    let portrait = document.createElement('img');
+  
+    // Change the textContent property of the h2, h3 and h4 elements to contain the prophet's full name, birthdate, and birthplace
+    h1.textContent = `${company.name}`;
+    h2.textContent = `${company.phoneNum}`;
+    h3.textContent = `${company.address}`;
+    h4.textContent = `${company.websiteURL}`;
+  
+    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values.
+    portrait.setAttribute('src', company.compLogo);
+    portrait.setAttribute('alt', `Company Logo for ${company.name}`);
+    portrait.setAttribute('loading', 'lazy');
+
+    // Add/append the section(card) with the h2, h3, and h4 element
+    card.appendChild(portrait);
+    card.appendChild(h1);
+    card.appendChild(h2);
+    card.appendChild(h3);
+    card.appendChild(h4);
+  
+    // Add/append the existing HTML div with the cards class with the section(card)
+    document.querySelector('div.cards').appendChild(card);
+}
---
+++ chamber/js/toggleGridList.js
@@ -0,0 +1,13 @@
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector(".cards");
+
+gridbutton.addEventListener("click", () => {
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", () => {
+	display.classList.add("list");
+	display.classList.remove("grid");
+});
