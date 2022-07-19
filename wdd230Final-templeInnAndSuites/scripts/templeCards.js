bc81c3d69fbc9aa7217d384096b3c71092bb1433
DubsOchenta <tylorjperry@gmail.com>
2022-07-18 20:51:29 -0700

    updates to multi sites and js and css

--- wdd230Final-templeInnAndSuites/scripts/templeCards.js
+++ wdd230Final-templeInnAndSuites/scripts/templeCards.js
@@ -19,6 +19,7 @@ const displayTemples = (temple) => {
     let h3 = document.createElement('h3');
     let h4 = document.createElement('h4');
     let like = document.createElement('div')
+    let counter = document.createElement('div')
     let portrait = document.createElement('img');
   
     // Change the textContent property of the h2, h3 and h4 elements to contain company name, phone number, address, and website URL.
@@ -26,9 +27,11 @@ const displayTemples = (temple) => {
     h2.textContent = `${temple.telephone}`;
     h3.textContent = `${temple.address}`;
     h4.textContent = `${temple.history}`;
+    counter.textContent = `Likes: ${localStorage.getItem('heartClicks')}`;
     
-    //Add Class to Like
+    //Add Class to like and counter
     like.classList.add('like', 'like-no');
+    counter.classList.add('counter');
   
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values.
     portrait.setAttribute('src', temple.image);
@@ -38,6 +41,7 @@ const displayTemples = (temple) => {
     // Add/append the section(card) with the h2, h3, and h4 element
     card.appendChild(portrait);
     card.appendChild(like);
+    card.appendChild(counter);
     card.appendChild(h1);
     card.appendChild(h2);
     card.appendChild(h3);
@@ -48,18 +52,19 @@ const displayTemples = (temple) => {
 };
 
 const listenForLikes = () => {
-  const likes = document.querySelectorAll('.like');
-  likes.forEach(like => {
-      like.addEventListener('click', (event) => {
-          event.target.classList.toggle('like-no');
-          event.target.classList.toggle('like-yes');
-          if (event.target.classList.contains('like-yes')) {
-              console.log('Saved Favorite 💾✅');
-              // getFaveData(event.target);
-          } else {
-              console.log('Removed Favorite 💾❌');
-              // getFaveData(event.target);
-          }
+    const likes = document.querySelectorAll('.like');
+    let count = Number(window.localStorage.getItem('likes'));
+    likes.forEach(like => {
+        like.addEventListener('click', (event) => {
+            event.target.classList.toggle('like-no');
+            event.target.classList.toggle('like-yes');
+            count++;
+            if (event.target.classList.contains('like-yes')) {
+                console.log('Saved Favorite 💾✅');
+                localStorage.setItem('heartClicks', count);
+            } else {
+                console.log('Removed Favorite 💾❌');
+            }
         })
     })
 };
