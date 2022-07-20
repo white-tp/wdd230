a15818d42b42ba034e6b735beab17523bac958d4
DubsOchenta <tylorjperry@gmail.com>
2022-07-19 20:05:45 -0700

    content update for 5 pages and css&js

--- wdd230Final-templeInnAndSuites/scripts/thankYou.js
+++ wdd230Final-templeInnAndSuites/scripts/thankYou.js
@@ -2,7 +2,7 @@ const feedbackElement = document.getElementById('feedback');
 const formElement = document.forms[0];
 formElement.addEventListener('submit', (e) => {
     e.preventDefault();
-    feedbackElement.innerHTML = `Hello ${formElement.fname.value}! Thank you for your reservation. We hope you have a wonderful stay with us!`;
+    feedbackElement.innerHTML = `Hello ${formElement.name.value}! Thank you for your question. We will get back to you with an answer shortly!`;
     feedbackElement.style.display = "block";
     document.body.classList.toggle('moveDown');
 });
